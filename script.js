0415457b869da74b2fadc74a85daec0043eb939d
shahmansoor1111 <shahmansoor0316@gmail.com>
2025-08-24 21:54:00 +0500

    ok

---
+++ script.js
@@ -0,0 +1,726 @@
+// if (1 == "1") {
+//     alert("aw tek da")
+// }
+// else{
+//     alert("ghalat de")
+// }
+// let age = 30;
+// age = 40;
+// alert(age)
+// var marks = 40;
+// marks = marks + 30;
+// alert(marks);
+// let box = 20;
+// let newBox = box + 10;
+// alert(newBox);
+// let tofeeBox = 20;
+// let newBox = tofeeBox + 10;
+// alert(newBox);
+// let marks = 30;
+// let assingnment = "110";
+// // alert(marks + assingnment);
+// let firstName = "shah";
+// let secondName = "mansoor"
+// let fullName = firstName + " " + secondName;
+// alert(fullName);
+// let box = 20;
+// box = box + 10;
+// alert(box);
+// let box = 110;
+// let newBox = 22;
+// let fullBox = box +  newBox;
+// alert(fullBox);
+// {
+//      secret = "leak my secret"
+
+// }
+// {
+//     var anotherSecret = "khair de marha"
+   
+// }
+
+// {
+//     var fullSecret = secret + " " + anotherSecret;
+//     alert(fullSecret);
+// }
+// if (1 === "1") {
+//     alert("tek da");
+// }
+// else{
+//     alert("ghalat de");
+// }
+// const bike = "my bike"
+// const Bike = "bro bike"
+// const bikE = "sis bike"
+// console.log(bike)
+// console.log(Bike)
+// console.log(bikE)
+
+// const number1 = 3;
+// const number2 = 5;
+// const result = number1 + number2;
+// alert(result);
+// let salary = 20;
+// const result = ++salary;
+// alert(result);
+// var salary = 33;
+// alert(++salary)
+// let salary = 34;
+// const increment = ++salary;
+// alert(increment);
+// let salary = 34;
+// const increment = salary++;
+// alert(salary);
+// let salary = 3;
+// alert(++salary)
+// let salary = 23;
+// const box = salary++;
+// alert(salary)
+// let salary = 20;
+// const box = ++salary;
+// alert(box);
+// let charge = 30;
+// alert(--charge);
+// let charge = 40;
+// const chargee = charge--;
+// alert(charge);
+// const age = prompt("Enter your age")
+// var firstName = prompt("enter your first name");
+// var secondName = prompt("enter your last name");
+// const age = prompt("Enter your age");
+// console.log("your current age is. " + age)
+// const name = prompt("your name plx")
+// console.log("my name is " + name );
+// const firstName = prompt("enter your first name");
+// const lastName = prompt("enter your last name");
+// const fullBox = (firstName + lastName);
+// alert("Hello my dear" + " " + fullBox);
+// const pen = +prompt("Price for pencil");
+// const marker = +prompt("Price for marker");
+// const bill = pen + marker ;
+// alert("Your bill is" + " " + bill);
+// const badal = false;
+// if (badal) {
+//     alert("olamaba");
+// }
+// else{
+//     alert("ma lamba");
+// }
+// let age = prompt("Please enter your age");
+
+// if (age >= 18) {
+//     alert("yes your elligible");
+// }
+// if (age < 18) {
+//     alert("no child")
+// }
+// let balance = prompt("enter your card balance");
+// if (balance >= 55) {
+//     alert("get on the bus sir");
+// }
+// if (balance < 55) {
+//     alert("get out pasawa");
+// }
+// let username = prompt("name olika");
+// let number = prompt("1 number put ka positve ,negative,zero");
+// if (number == 0) {
+//     alert(username + " " + "zero de warko marha");
+// }
+// if (number > 0) {
+//     alert(username + " " + "good positive de warko manama de")
+// }
+// if (number < 0) {
+//     alert(username + " " + "negative ma warko")
+// }
+// const bhook = true;
+// const paisa = true;
+// if (bhook && paisa) {
+//     alert("you can eat the burger");
+// }
+// else{
+//     alert("oza marha");
+// }
+// const rain = !false;
+// if (rain) {
+//     alert("baran de");
+// }
+// else{
+//     alert("nishta")
+// }
+// const name = prompt("enter your name");
+// const number = +prompt("1 number dalo");
+// const result = number % 2;
+// if (result === 0) {
+//     alert("You put even number");
+// }
+// else{
+//     alert("you put odd number")
+// }
+// const time = true;
+// const money = true;
+// if(time && money) {
+//     alert("chkar la tlai she");
+// }
+// else{
+//     alert("chakar la nashi tlai");
+// }
+// const age = prompt("enter your age");
+// if (age >= 0) {
+//     alert("your are under 10");
+// }
+// if(age <= 10) {
+//     alert("you are under 20")
+// }
+// if(age <= 20) {
+//     alert("you are under 30")
+// }
+// const firstNumber = +prompt("enter the first number");
+// const secondNumber = +prompt("enter the second number");
+// const thirdNumber = +prompt("enter the third number");
+
+// if (firstNumber > secondNumber && firstNumber > thirdNumber) {
+//     alert("first number " + firstNumber + " " + "is greater than all");
+// }
+// else if (secondNumber > thirdNumber) {
+//     alert("second number " + secondNumber + " " + "is greater than all");
+// }
+// else{
+//     alert("third number " + thirdNumber + " " + "is greater than all");
+// }
+// const firstNumber = +prompt("enter your first number");
+// const secondNumber = +prompt("enter your second number");
+// const thirdNumber = +prompt("enter your third number");
+// if (firstNumber > secondNumber && firstNumber > thirdNumber) {
+//     alert("your first number " + firstNumber + " " + "is greater than all");
+// }
+// else if(secondNumber > thirdNumber) {
+//     alert("your second number " + secondNumber + " " + "is greater than all");
+// }
+// else{
+//     alert("your third number " + thirdNumber + " " + "is greater than all");
+// }
+// const userName = prompt("enter your name");
+// const grade = +prompt("enter your grade");
+// if (grade >= 90) {
+//     alert(userName + " " + "your grade is A++");
+// }
+// else if (grade >= 80) {
+//     alert(userName +  " " + "your grade is A+");
+// }
+// else if (grade >= 70) {
+//     alert(userName +  " " + "your grade is A");
+// }
+// else if (grade >= 60) {
+//     alert(userName +  " " + "your grade is normal");
+// }
+// else {
+    
+//         alert(userName +  " " + "sorry you are fail");
+    
+// // }
+// var cars = ["alto","markx", "mehran", "vidz"];
+// cars[3] = "markss2";
+// cars.shift()
+// cars.pop()
+// cars.push("GLI")
+// cars.unshift("newcar")
+// alert(cars);
+// const userName = prompt("enter your username");
+// let password = "";
+
+// if (userName === "admin") {
+// password = prompt("enter your password");
+// if (password === "secret")  {
+// document.write("yes password is right");
+// }
+// else{
+//     document.write("confirm your password");
+// }
+// else{
+//     document.write("you username is totaly wrong");
+// }
+// }
+// let age = +prompt("enter your age");
+// const student = +prompt('are you student\n0.No\n1.Yes');
+// if (age < 12) {
+//     alert("your are child give me 5 ruppes")
+// }
+//     else{
+//         if (age > 12 && age < 18 && student) {
+//             alert("your are student give me 20 rupes")
+//         }
+//         else{
+//             alert("you are not a adult give me 100 rupes");
+//         }
+//     }
+// const cleanCity = ["peshawar", "lahore", "karachi", "multan", "islamabad" ];
+// const check = prompt("enter your city").toLowerCase()
+
+// if(check === cleanCity[0]) {
+//     alert("it is the cleanest city")
+// }
+// else if (check === cleanCity[1]) {
+//     alert("it is the cleanest city")
+// }
+// else if (check === cleanCity[2]) {
+//     alert("it is the cleanest city")
+// }
+// else if (check === cleanCity[3]) {
+//     alert("it is the cleanest city")
+// }
+// else if (check === cleanCity[4]) {
+//     alert("it is the cleanest city")
+// }
+// else{
+//     alert("sta der ganda city da pasa ghualan")
+// }
+// for (let number = 1; number <= 10; number++) {
+//     console.log(number)
+// }
+// const cities = ["peshawar", "lahore", "karachi", "multan", "islamabad"];
+// const check = prompt("enter your city");
+
+// for (let index = 0; index < cities.length;index++ ) {
+//     if (check === cities[index]) {
+//         alert("yes its is clean")
+//     }
+// }
+// const cities = ["peshawar", "lahore", "karachi", "multan", "islamabad"]
+// const user = prompt("enter your city");
+
+// for (let i = 0; i < cities.length; i++) {
+//     if(user === cities[i]) {
+//         alert("your are clean")
+//     }
+// }
+// let firstNumber = 5;
+// let secondNumber = 10;
+// let sum = firstNumber + secondNumber;
+// console.log(`the sum of ${firstNumber} and ${secondNumber} is ${sum}`)
+// let count = 0;
+// for (let i = 1; i <= 3; i++) {
+//     for (let j = 1; j <= 3; j++) {
+//         console.log(`i: ${i} j: ${j} `)
+//     }
+// }
+// for (let i = 1; i <= 6; i++) {
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i * j }`)
+//     }
+
+// }
+// let user = +prompt("enter a number upto 10")
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${user} * ${i} = ${user * i}`)
+    
+// }
+// const universities = ["peshawar", "iqra", "cecos", "nust", "city"];
+// const user = prompt("enter your univercity").toLocaleLowerCase();
+// let elligible = false;
+// for (let i = 0; i < universities.length; i++) {
+//     if (user === universities[i]) {
+//      elligible = true
+//     console.log("you are ellegiblle")
+//     break;
+// }
+// }
+// if(!elligible){
+//     console.log("you are not ellible")
+// }
+// const first = prompt("enter your name");
+// const letter = first.slice(0,1)
+// const capital = letter.toUpperCase()
+// const remin = first.slice(1)
+// const result = capital + remin
+
+// alert(result)
+// const month = prompt("enter your birthmonth")
+
+// if (month.length > 3 ) {
+//     const short = month.slice(0,1)
+//     const remin = short.toUpperCase()
+//     const remain = month.slice(1)
+//     const result = remin + remain
+//     alert(result.slice(0,3))
+// }
+// else{
+//     const short = month.slice(0,1)
+//     const remin = short.toUpperCase()
+//     const remain = month.slice(1)
+//     const result = remin + remain
+//     alert(result)
+// }
+// const namee = "shahansoor"
+// const check = namee.lastIndexOf("s")
+// alert(check)
+// const password = prompt("enter your password")
+// const check = password.indexOf("@");
+// let valid = false
+// for (let i = 0; i < password.length; i++){
+//     if ("@" === password[i]) {
+//         valid = true
+//         break;
+//     }
+// }
+// if (valid) {
+//     alert("done")
+// }
+// else{
+//     alert("not done")
+// }
+// const password = prompt("enter your pasword")
+// const check = password.indexOf("@")
+
+// if (check === -1){
+//     alert("invalid password")
+// }
+// else{
+//     alert("valid")
+// }
+// const a = "shahmansoor is very good boy and also good heart"
+// const check = a.replace(/good/g,)
+// alert(check)
+// let a = Math.round(1.6)
+// let a = Math.floor(3.9)
+// let a = Math.ceil(3.1)
+// alert(a)
+// let number = Math.random() * 3
+// let remain = Math.ceil(number)
+// alert(remain)
+// const user = +prompt("select one number upto 5")
+// const random = Math.ceil (Math.random() * 5);
+
+
+// if (user === random) {
+//     alert("yes you are right ")
+// }
+// else{
+//     alert("you are wrong")
+// }
+
+// alert("the number was" + " " + random)
+// const user = +prompt("enter your age")
+// const ellegiblle = user -18 
+// alert(ellegiblle)
+// alert("your id card made" + " " + ellegiblle + " " + "ago")
+// const random = Math.ceil(Math.random( ) * 3);
+// const  user = +prompt("enter one number upto three")
+
+// if (user === random) {
+//     alert("yes you are right")
+// }
+// else{
+//     alert("not right")
+// }
+// alert("the number was" + " " + random)
+// const obtained = 633
+// const total = 1100
+// const result = ((obtained/total) * 100).toFixed(2)
+// alert(result)
+
+// const userr = prompt("enter your name")
+// alert(userr + " " + "plz ok to check your seat")
+// const seat = Math.ceil(Math.random() * 100)
+
+// alert(userr + " " + "your seat is" + " " + seat)
+// const namee = prompt("enter your name")
+// const first = namee.slice(0, 1)
+// const uper = first.toUpperCase()
+// const baki = namee.slice(1)
+// const result = uper + baki
+// alert(result)
+// const user = prompt("Type your full name").toLowerCase()
+// const random = Math.ceil(Math.random( ) * 100 );
+// alert("If your number will 50 we will give you a fascinating prize")
+// alert(user + " " + "Your number is" + " " + random)
+
+// if (random === 50) {
+//     alert(user + " " + "You won the price")
+// }
+// else{
+//     alert(user + " " + "Sorry! Better luck for next time")
+// }
+// const user = prompt("Enter your and your friend name to check the relationship ")
+// const random = Math.ceil(Math.random( ) * 100);
+
+// if (random > 90 ) {
+//     alert("Your are two best friends")
+// }
+// else if (random >= 50 && random < 90) {
+//     alert("Your are normal friends")
+// }
+// else{
+//     alert("You are snake friends hhhhhhh")
+// }
+// const daysName = ["sunday", "monday", "tuesday", "wednesday", "thursday","friday","saturday",]
+// const check = new Date().getDay();
+
+// let result = daysName[check]; 
+
+
+// console.log(result)
+
+// let millisecond = 1739791410462
+// let second = millisecond/1000
+// let min = Math.floor(second/60)
+// let hour = Math.floor(min/60)
+// let day = Math.floor(hour/24)
+// console.log(day)
+// let days = ["sunday", "monday", "tuesday", "wednesday", "thursday","friday","saturday"]
+// let check = new Date().getDay()
+// let result = days[check]
+// console.log(result)
+
+
+// const rightNow = new Date()
+// // console.log(rightNow)
+
+// const currentYear = rightNow.getDay()
+// console.log(currentYear)
+// let dayS = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+// let now = new Date() 
+// let check = nuMber.getDay()
+// let result = dayS[check]
+// console.log(result)
+// const now = new Date()
+// const hOur = now.getHours()
+// const Mins = now.getMinutes()
+// console.log({hOur , Mins})
+// const houR = nuMber.getHours()
+// const Mins = nuMber.getMinutes()
+
+// if (houR > 11 ) {
+//     console.log(`${houR} ${Mins} pm`)
+// }
+// else{
+//     console.log(`${houR} ${Mins} am`)
+// }
+// const fazal = new Date("april 6, 2025")
+// const sumra = fazal - now 
+// console.log(Math.floor(sumra / (1000 * 60 * 60 * 24)))
+
+// const mybirhtday = new Date("october 21, 2003")
+// const check = now - mybirhtday
+// console.log(Math.floor(check / (1000 * 60 * 60 * 24 * 365)))
+
+// function greetings (userName) {
+//     alert("hello" + " " + userName)
+// }
+// greetings("shahmansoor")
+// greetings("khan")
+
+
+// function greetings (userName) {
+//     alert("hello" + " " + userName)
+// }
+// const firstName = "shahmansoor"
+// const lastName = "khan "
+// greetings (firstName)
+// greetings (lastName)
+//  function userDetails (namee, age, married) {
+//     alert(`${namee} ${age} ${married}`)
+//  }
+//  const nAme = "shahmansoor"
+//  const aGe = 30
+//  const marrieD = "marrrried"
+//  userDetails (nAme + aGe + marrieD)
+
+// const namee = "shahmansoor"
+
+// function Name () {
+//     document.write(namee)
+// }
+// document.write(namee + "<br>")
+
+// Name()
+
+// ////////     110 /////////
+
+// let x = "monday"
+
+// switch (x) {
+//     case "monday":
+//     console.log("yes today is monday");
+//     break;
+//     case "tuesday":
+//     console.log("yes today is tuesday");
+//     break;
+//     default:
+//     console.log ("its nor monday or tuesday");
+// }
+// function greet(){
+//     alert("helllo")
+// }
+
+// function checkEmail (field) {
+//     if(document.getElementById(field).value === '') {
+//         alert("email is required")
+//     }
+//     else{
+//         alert("done")
+//     }
+// }
+// ////////   132 ///////////
+
+
+// function zipp () {
+//     var zips = +document.getElementById("zipcode").value
+//     var arEa = document.getElementById("location")
+// if (zips === 1) {
+//     arEa.value = "shahlam"
+// }
+// else if (zips === 2) {
+//     arEa.value = "mangar"
+// }
+// else if (zips === 3) {
+//     arEa.value = "babuzai"
+// }
+// else if (zips === 4) {
+//     arEa.value = "takhtabad"
+// }
+// else if (zips === 5) {
+//     arEa.value = "khazna"
+// }
+// else{
+//     alert("invalid")
+// }
+// }
+/////   OBJECTS//////
+// let car = {
+//     model : "mehran",
+//     number : "islamabad",
+//     code : 3334,
+// }
+// console.log(car["model"])
+// let car = {}
+// car.model = "mehran"
+// car.speed = 220
+// car["color"] = "red"
+// car.color = "white"
+// console.log(car)
+
+//// constructor function ////
+// / function Car (color,model,year) {
+//     this.color = color;
+//     this.model = model;
+//     this.year = year;
+//     // this.start = function () {
+//     //     console.log(this.model + " "  + " has started")
+//     // }
+// }
+// var car = new Car("black", "bmw", 2020)
+// console.log(car)
+
+// // car.start()
+// function Person(firstname, lastname, age) {
+//     this.pela = firstname;
+//     this.dosra = lastname;
+//     this.umar = age;
+
+// }
+// var ali = new Person ("shah" , "mansoor", 45)
+// var jawad = new Person ("jwad" , "ali", 55)
+// var imtyaz = new Person ("imtyaz" , "khan", 15)
+// console.table([ali, jawad, imtyaz])
+///// prototypes ////
+
+// function Person (name , age) {
+//     this.name = name;
+//     this.age = age
+
+// }
+// Person.prototype.greet = function () {
+//     console.log("hello i am " + this.name + " " + "and i am " + this.age + " " + "years old" )
+// }
+
+// var person1 = new Person("shahmansoor" , 32)
+// var person2 = new Person("faraz" , 31)
+
+// var person3 = new Person("wahab" , 42)
+// var person4 = new Person("ghulam" , 32)
+// person1.greet()
+// person2.greet()
+// person3.greet()
+// person4.greet()
+
+// var car = {
+//     color : "red", 
+//     model : "mehran"
+// };
+// // console.log("model" in car)
+// console.log(car.hasOwnProperty ("color") )
+if(car.model === undefined) {
+    alert("nishta paki")
+}
+else{
+    alert("shta paki")
+}
+ // getting url ///
+var ne = window.location.href;
+var me = window.location.pathname;
+var he = window.location.hostname;
+window.location.href = "https://google.com"
+
+console.log(ne)
+
+/// map reduce filter/////
+
+let arr = [1,2,3,4,5,6,7];
+let each = arr.forEach((element) => {
+    console.log(element * element) 
+});
+let arr = [1,2,3,4,5,6,7];
+let each = arr.map((element) => {
+    return element * element
+});
+console.log(each)
+let arr = [1,2,3,4,5,6,7];
+
+// for (let i of arr) {
+//     console.log(arr[i])
+// }
+// function Person(name,age,color,) {
+//     this.name = name;
+//     this.age = age;
+//     this.colour = color
+//     this.greet = function () {
+//        console.log ("hello i am shah")
+//     }
+// }
+// let mansoor = new Person("shahmansoor", 12, "green")
+// // console.log(mansoor)
+// mansoor.greet()
+// let a = [1,22,3,42,5,6,77,]
+
+// let b = a.filter((element) =>{
+//     return element > 10
+// })
+// console.log(b)
+// let a = [1,2,3,2,5,6,7,]
+
+// let b = a.reduce((bojai, old) =>{
+//     return bojai + old
+// },2)
+// console.log(b)
+
+
+
+// let a = prompt("enter uour city name to check")
+
+// let arr = ["peshawar", "lahore","multan"]
+
+// let check = arr.forEach((element) => {
+//     if (element === a) {
+      
+//         alert("yes")
+        
+//     }
+//     else{
+//        alert ("no")
+//     }  break; 
+// })
+
+let a;
+let b = null
+console.log(typeof !a)
